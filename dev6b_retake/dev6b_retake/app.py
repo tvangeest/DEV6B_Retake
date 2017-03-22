316bc27c727c8f49cf9cd21fd68750d4ecd92e27
tvangeest <tommygeest@hotmail.com>
2017-03-22 16:02:03 +0100

    Controller working

--- dev6b_retake/dev6b_retake/app.py
+++ dev6b_retake/dev6b_retake/app.py
@@ -3,7 +3,8 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, url_for
+from controller import *
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -12,7 +13,15 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def index():
+    return Controller.index()
 
+@app.route('/a1', methods=['GET', 'POST'])
+def a1():
+    return Controller.a1()
+
+@app.route('/a2', methods=['GET', 'POST'])
+def a2():
+    return Controller.a2()
 
 if __name__ == '__main__':
     import os
---
+++ dev6b_retake/dev6b_retake/controller.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+class Controller:
+
+  def index(): 
+    return render_template('index.html')
+
+  def a1():
+    if request.method == 'GET':
+      return render_template('A1.html')
+  
+  def a2():
+    if request.method == 'GET':
+      return render_template('A2.html')
