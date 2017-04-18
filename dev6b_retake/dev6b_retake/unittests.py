23938f557236a9aa511487fbbeff31069c4de86e
tvangeest <tommygeest@hotmail.com>
2017-04-18 15:05:27 +0200

    changes

---
+++ dev6b_retake/dev6b_retake/unittests.py
@@ -0,0 +1,33 @@
+import unittest, json
+from flask import *
+import unittest
+from app import app
+
+class Test_unittests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_statusA1(self):
+        result = self.app.get('/a1')
+        self.assertEqual(result.status_code,200)
+
+    def test_statusA2(self):
+        result = self.app.get('/a2')
+        self.assertEqual(result.status_code,200)
+
+    def test_statusindex(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code,200)
+
+    def test_jsona1(self):
+        result = self.app.get('/a1answers.json')
+        self.assertEqual(result.status_code,200)
+  
+    def test_jsona2(self):
+        result = self.app.get('/a2answers.json')
+        self.assertEqual(result.status_code,200)
+
+
+if __name__ == '__main__':
+    unittest.main()
